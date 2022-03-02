fe69619d89c4283d85de9a5c7baf2ed77a569f5d
LaValle03 <72546440+LaValle03@users.noreply.github.com>
2022-03-02 15:47:01 +0100

    Update app.py

--- cookies/app.py
+++ cookies/app.py
@@ -1,6 +1,100 @@
 from flask import Flask, render_template, request, make_response, redirect, url_for
 import sqlite3
 from datetime import datetime
+import RPi.GPIO as GPIO
+
+class AlphaBot(object):
+    def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
+        self.IN1 = in1
+        self.IN2 = in2
+        self.IN3 = in3
+        self.IN4 = in4
+        self.ENA = ena
+        self.ENB = enb
+        self.PA  = 50
+        self.PB  = 48
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA,500)
+        self.PWMB = GPIO.PWM(self.ENB,500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()
+
+    def forward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+
+    def stop(self):
+        self.PWMA.ChangeDutyCycle(0)
+        self.PWMB.ChangeDutyCycle(0)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def backward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def left(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+
+    def right(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        
+    def set_pwm_a(self, value):
+        self.PA = value
+        self.PWMA.ChangeDutyCycle(self.PA)
+
+    def set_pwm_b(self, value):
+        self.PB = value
+        self.PWMB.ChangeDutyCycle(self.PB)    
+        
+    def set_motor(self, left, right):
+        if (right >= 0) and (right <= 100):
+            GPIO.output(self.IN1, GPIO.HIGH)
+            GPIO.output(self.IN2, GPIO.LOW)
+            self.PWMA.ChangeDutyCycle(right)
+        elif (right < 0) and (right >= -100):
+            GPIO.output(self.IN1, GPIO.LOW)
+            GPIO.output(self.IN2, GPIO.HIGH)
+            self.PWMA.ChangeDutyCycle(0 - right)
+        if (left >= 0) and (left <= 100):
+            GPIO.output(self.IN3, GPIO.HIGH)
+            GPIO.output(self.IN4, GPIO.LOW)
+            self.PWMB.ChangeDutyCycle(left)
+        elif (left < 0) and (left >= -100):
+            GPIO.output(self.IN3, GPIO.LOW)
+            GPIO.output(self.IN4, GPIO.HIGH)
+            self.PWMB.ChangeDutyCycle(0 - left)
+
+Ab = AlphaBot()
 
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
@@ -40,18 +134,23 @@ def controlloRobot():
 
         if request.form.get('avanti') == 'avanti':
             print("Avanti")
+            Ab.forward()
             cur.execute(f"INSERT INTO comandi (utente, comando, data) VALUES ('{request.cookies.get('username')}','avanti','{dt_string}')")
         elif  request.form.get('indietro') == 'indietro':
             print("Indietro")
+            Ab.backward()
             cur.execute(f"INSERT INTO comandi (utente, comando, data) VALUES ('{request.cookies.get('username')}','indietro','{dt_string}')")
         elif  request.form.get('sinistra') == 'sinistra':
             print("Sinistra")
+            Ab.left()
             cur.execute(f"INSERT INTO comandi (utente, comando, data) VALUES ('{request.cookies.get('username')}','sinistra','{dt_string}')")
         elif  request.form.get('destra') == 'destra':
             print("Destra")
+            Ab.right()
             cur.execute(f"INSERT INTO comandi (utente, comando, data) VALUES ('{request.cookies.get('username')}','destra','{dt_string}')")
         elif  request.form.get('stop') == 'stop':
             print("Stop")
+            Ab.stop()
             cur.execute(f"INSERT INTO comandi (utente, comando, data) VALUES ('{request.cookies.get('username')}','stop','{dt_string}')")
 
     con.commit()
