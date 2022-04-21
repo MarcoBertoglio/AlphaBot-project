c32694c640835e29255a09cdda621e6f0c2a7a8a
LaValle03 <72546440+LaValle03@users.noreply.github.com>
2022-04-21 10:11:33 +0200

    Update client.py

--- comando_automatico_sensori/client.py
+++ comando_automatico_sensori/client.py
@@ -2,16 +2,18 @@ import requests
 from flask import jsonify
 import time
 
+#Lista con i valori dei sensori del Robot
 sensori = [1,1]
 while True:
     pwml = 0
     pwmr = 0
-
+    
+    #Il client richiede i valori dei sensori alla WEBPI
     r = requests.get("http://192.168.0.138:5000//api/v1/sensors/obstacles").text
 
     sensori[0] = r[0]
     sensori[1] = r[2]
-
+    
     if (sensori[0] == "1" and sensori[1] == "1"):
         print("AVANTI")
         pwml = -40
@@ -24,6 +26,7 @@ while True:
         print("DESTRA")
         pwml = -40
         pwmr = -20
-
+    
+    #Manda una GET alla WEBPI con i valori dei sensori
     r = requests.get(f"http://192.168.0.138:5000//api/v1/motors/both?pwml={pwml}&pwmr={pwmr}&tempo={0.2}")
     time.sleep(0.3)
